b2ddd7de97d2cdbe0294f397fa940b672bd13185
Alexandre SERMESSE <alexandre.sermesse@viacesi.fr>
2016-06-17 09:44:08 +0200

    BLOKCING/PENETRABLE

--- model/src/main/java/elements_fixes/Crystal_ball.java
+++ model/src/main/java/elements_fixes/Crystal_ball.java
@@ -6,7 +6,7 @@ import element.Sprite;
 class Crystall_ball extends element_fixe {
 
 	public Crystall_ball() {
-		super(new Sprite("images/Crystall_ball.png"), Permeability.BLOCKING, 'O');
+		super(new Sprite("images/Crystall_ball.png"), Permeability.PENETRABLE, 'O');
 	}
 
 }
